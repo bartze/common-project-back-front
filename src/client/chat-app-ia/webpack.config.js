532ffbdd739b7bb29863df50bb9162ee26f8b6d8
null <jbucklandevelopment@icloud.com>
2024-08-14 10:30:34 +0200

    update(chore): force all apps to work again after review chat and weboack config

--- src/client/chat-app-ia/webpack.config.js
+++ src/client/chat-app-ia/webpack.config.js
@@ -4,38 +4,40 @@ import path from 'path';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-export default {
-    mode: 'development',
-    devtool: 'source-map',
-    entry: {
-        'dist/main': './main.js',
-        'dist/main.min': './main.js',
-    },
-    output: {
-        filename: '[name].js',
-        path: __dirname,
-        library: {
-            type: 'module',
-        },
-    },
-    plugins: [
-        // Copy .wasm files to dist folder
-        new CopyWebpackPlugin({
-            patterns: [
-                {
-                    from: 'node_modules/onnxruntime-web/dist/*.jsep.*',
-                    to: 'dist/[name][ext]'
-                },
-            ],
-        }),
-    ],
-    devServer: {
-        static: {
-            directory: __dirname
-        },
-        port: 8080
-    },
-    experiments: {
-        outputModule: true,
-    },
+const config = {
+	mode: 'development',
+	devtool: 'source-map',
+	entry: {
+		'dist/main': './main.js',
+		'dist/main.min': './main.js',
+	},
+	output: {
+		filename: '[name].js',
+		path: __dirname,
+		library: {
+			type: 'module',
+		},
+	},
+	plugins: [
+		// Copy .wasm files to dist folder
+		new CopyWebpackPlugin({
+			patterns: [
+				{
+					from: 'node_modules/onnxruntime-web/dist/*.jsep.*',
+					to: 'dist/[name][ext]',
+				},
+			],
+		}),
+	],
+	devServer: {
+		static: {
+			directory: __dirname,
+		},
+		port: 8084,
+	},
+	experiments: {
+		outputModule: true,
+	},
 };
+
+export default config;
