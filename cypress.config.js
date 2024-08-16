39cae26dd89bc6e5a042f2985bb7980e805f4be0
null <jbucklandevelopment@icloud.com>
2024-08-16 11:51:34 +0200

    added(e2e-coverage): added coverage basics for e2e with html report

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
+	e2e: {
+		setupNodeEvents(on, config) {
+			// implement node event listeners here
+			require('@cypress/code-coverage/task')(on, config);
+			return config;
+		},
+	},
 });
